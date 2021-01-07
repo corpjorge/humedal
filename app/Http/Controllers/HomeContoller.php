f419701ac6be29a06e70f160bdecddd3d6c1a155
Jorge Eduardo <corpjorge@hotmail.com>
2021-01-07 17:08:26 -0500

    vue js

--- app/Http/Controllers/HomeContoller.php
+++ app/Http/Controllers/HomeContoller.php
@@ -8,10 +8,6 @@ class HomeContoller extends Controller
 {
     public function index()
     {
-        $miami   = Http::get('http://humedal.test:8080/api/weather/miami');
-        $orlando = Http::get('http://humedal.test:8080/api/weather/orlando');
-        //$newyork = Http::get('http://humedal.test:8080/api/weather/newyork');
-
-        //return view('welcome', [ 'miami' => $miami, 'orlando' => $orlando, 'newyork' => $newyork ]);
+        return view('welcome');
     }
 }
